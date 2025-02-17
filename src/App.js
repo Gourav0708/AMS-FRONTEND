dd45f6532207461519e2bca3e489aea799bf2e0e
Gourav <renox0721@gmail.com>
2025-02-17 22:36:55 +0530

    Frontend AMS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import SignUP from './Componenets/SignUP'
+import LoginPage from './Componenets/LoginPage'
+import Dashboard from './Componenets/Dashboard';
+import { ToastContainer} from 'react-toastify'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+
+} from "react-router-dom";
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ToastContainer/>
+    <Router>
+    <Routes>
+      <Route exact path ="/signup" element={<SignUP category="SignUp"/>}/>
+      <Route exact path ="/" element={<LoginPage category="Login"/>}/>
+      <Route exact path="/dashboard" element={<Dashboard key="Dashboard" category="Dashboard" />} />
+      </Routes>
+      </Router>
+    </>
+
   );
 }
 
